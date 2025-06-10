9f7e4895b4da52b440045d41e304c2addb8bef0e
Jaqueline Batista Vieira✭・ <jackebatista07@hotmail.com>
2025-06-10 16:05:28 -0300

    Add files via upload

--- test_funcoes.py
+++ test_funcoes.py
@@ -1,8 +1,14 @@
+# Aqui estamos "importando" as funções que foram criadas no outro arquivo chamado funcoes.py
+# Assim podem ser usadas essas funções nesse arquivo de testes.
 from funcoes import verifica_email, validar_senha, calcular_media, verificar_maior_idade, eh_positivo, status_aluno
 
+# Essa função testa se a função verifica_email() está funcionando direito.
 def test_verifica_email():
+    # Aqui testamos um e-mail que deve estar certo. Se a função retornar "email válido", o teste passa.
     assert verifica_email('teste@gmail.com') == 'email válido'
+    # Aqui testamos um e-mail que está errado (falta o "@"). Então é esperado que a função diga que está "incorreto" no terminal.
     assert verifica_email('teste.com') == 'email incorreto'
+
 
 def test_validar_senha():
     assert validar_senha('Senha123@') == 'senha válida'
